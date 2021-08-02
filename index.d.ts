5043c1210666f975b24402ace12ea3ba53f1b496
Malte Hallström <malte.hallstrom@gmail.com>
2021-08-02 22:54:03 +0200

    docs: add JSDoc comment to TS definition

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,8 @@
 declare module "@navigraph/pkce" {
+  /**
+   * Generates a Proof Key for Code Exchange (PKCE) challenge pair.
+   * @see https://datatracker.ietf.org/doc/html/rfc7636
+   */
   export default function pkce(): {
     code_challenge: string;
     code_verifier: string;
