de611c30ac97e289bb1879944b606387561d3b3e
Malte Hallström <malte.hallstrom@gmail.com>
2021-08-02 22:29:05 +0200

    test: prettify tests, test throwing of error

--- __tests__/pkce.spec.js
+++ __tests__/pkce.spec.js
@@ -1,60 +1,78 @@
 const pkce = require("../index");
-const crypto = require('crypto');
+const crypto = require("crypto");
 
-Object.defineProperty(global.self, 'crypto', {
+Object.defineProperty(global.self, "crypto", {
   value: {
-    getRandomValues: arr => crypto.randomBytes(arr.length)
-  }
+    getRandomValues: (arr) => crypto.randomBytes(arr.length),
+  },
 });
 
 describe("@navigraph/pkce", () => {
+  test("verifier length is 43", () => {
+    expect(pkce().code_verifier.length).toEqual(43);
+  });
 
-    test("verifier length is 43", () => {
-      expect(pkce().code_verifier.length).toEqual(43);
-    });
+  test("challenge length is 43", () => {
+    expect(pkce().code_challenge.length).toEqual(43);
+  });
 
-    test("challenge length is 43", () => {
-      expect(pkce().code_challenge.length).toEqual(43);
-    });
+  test("code_verifier pattern matches", () => {
+    const pattern = /^[A-Za-z\d\-._~]{43,128}$/;
+    const challengePair = pkce();
+    expect(challengePair.code_verifier).toMatch(pattern);
+  });
 
-    test("code_verifier pattern matches", () => {
-      const pattern = /^[A-Za-z\d\-._~]{43,128}$/;
-      const challengePair = pkce();
-      expect(challengePair.code_verifier).toMatch(pattern);
-    });
+  test("code_verifier doesn't have [=+/]", () => {
+    const challengePair = pkce();
+    expect(challengePair.code_verifier).toEqual(
+      expect.not.stringContaining("=")
+    );
+    expect(challengePair.code_verifier).toEqual(
+      expect.not.stringContaining("+")
+    );
+    expect(challengePair.code_verifier).toEqual(
+      expect.not.stringContaining("/")
+    );
+  });
 
-    test("code_verifier doesn't have [=+/]", () => {
-      const challengePair = pkce();
-      expect(challengePair.code_verifier).toEqual(expect.not.stringContaining("="))
-      expect(challengePair.code_verifier).toEqual(expect.not.stringContaining("+"))
-      expect(challengePair.code_verifier).toEqual(expect.not.stringContaining("/"))
-    });
+  test("code_challenge doesn't have [=+/]", () => {
+    const challengePair = pkce();
+    expect(challengePair.code_verifier).toEqual(
+      expect.not.stringContaining("=")
+    );
+    expect(challengePair.code_verifier).toEqual(
+      expect.not.stringContaining("+")
+    );
+    expect(challengePair.code_verifier).toEqual(
+      expect.not.stringContaining("/")
+    );
+  });
 
-    test("code_challenge doesn't have [=+/]", () => {
-      const challengePair = pkce();
-      expect(challengePair.code_verifier).toEqual(expect.not.stringContaining("="))
-      expect(challengePair.code_verifier).toEqual(expect.not.stringContaining("+"))
-      expect(challengePair.code_verifier).toEqual(expect.not.stringContaining("/"))
-    });
+  test("code_challenge to be base64 uri-encoded sha256(code_verifier)", () => {
+    function base64UrlEncode(base64) {
+      return base64.replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
+    }
 
-    test("code_challenge to be base64 uri-encoded sha256(code_verifier)", () => {
+    function generateChallenge(verifier) {
+      const hash = crypto
+        .createHash("sha256")
+        .update(verifier)
+        .digest("base64");
+      return base64UrlEncode(hash);
+    }
 
-      function base64UrlEncode(base64) {
-        return base64
-          .replace(/=/g, "")
-          .replace(/\+/g, "-")
-          .replace(/\//g, "_");
-      }
+    const challengePair = pkce();
+    expect(challengePair.code_challenge).toEqual(
+      generateChallenge(challengePair.code_verifier)
+    );
+  });
 
-      function generateChallenge(verifier) {
-        const hash = crypto.createHash("sha256")
-          .update(verifier)
-          .digest("base64");
-        return base64UrlEncode(hash);
-      }
+  test("throws when invoked in non-browser environment", () => {
+    windowSpy = jest.spyOn(window, "window", "get");
+    windowSpy.mockImplementation(() => undefined);
 
-      const challengePair = pkce();
-      expect(challengePair.code_challenge).toEqual(generateChallenge(challengePair.code_verifier))
-    });
-
+    expect(pkce).toThrowError(
+      "This code is only meant to run in a browser environment."
+    );
+  });
 });
